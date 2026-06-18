0f5c3e3c9efb91fc78a643821ac02077098ef4a4
Lead Keeper Admin <admin@econos.io>
2026-06-18 09:51:12 -0400

    fix: relajar CORS para diagnostico

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,7 @@ app.use(helmet({
     contentSecurityPolicy: false
 }));
 app.use(cors({
-    origin: ['https://leedkeeper.econos.io', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
